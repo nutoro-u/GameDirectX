98763a0f5bc98f1a0dc0432647f0620fd3dfa6da
mezanix <mezanixissa@gmail.com>
2026-07-02 22:13:15 +0200

    DX core Base class created

---
+++ DX3D/Include/DX3D/Core/Base.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace DX3D
+{
+	class Base
+	{
+		public:
+			Base();
+			virtual ~Base();
+	};
+} // namespace DX3D
---
+++ DX3D/Source/DX3D/Core/Base.cpp
@@ -0,0 +1,9 @@
+#include <DX3D/Core/Base.h>
+
+DX3D::Base::Base()
+{
+}
+
+DX3D::Base::~Base()
+{
+}
